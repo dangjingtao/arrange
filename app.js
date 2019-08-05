a468464e8aed2944ee029ca35dfa23af3db0fdcb
dangjingtao <dangjingtao@163.com>
2019-08-05 11:24:39 +0800

    api

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const Koa=require('Koa')
+const Koa=require('koa')
+const Router=require('koa-router')
 const app =new Koa();
 
-app.use(async (ctx,next)=>{
-    Math.random()>0.9?aaa():2
-    await next();
-    ctx.response.type='text/html';
-    ctx.response.body='<h1>hello</h1>';
-})
+const router=new Router();
+
+router.get('/', (ctx, next) => {
+    // ctx.router available
+    ctx.body='<h1>hello</h1>'
+});
+
+
+router.get('/api', (ctx, next) => {
+    // ctx.router available
+    ctx.body={
+        data:[1,2,3]
+    }
+});
+
+
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 if(!module.parent){
     app.listen(3000,()=>{
